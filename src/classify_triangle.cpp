3c6fb589ac0214abfaf48af334479681d0136bca
Kyle M. Hart <kylemhart@gmail.com>
2022-01-04 19:20:41 -0500

    Unit Test (#2)
    * Add unit tests
    
    * Fix code to pass tests
    
    * Fix typo in comment

--- src/classify_triangle.cpp
+++ src/classify_triangle.cpp
@@ -2,12 +2,24 @@
 
 namespace classify_triangle {
 TriangleType classifyTriangle(double side1, double side2, double side3) {
+  // First, check that all numbers are positive.
+  if (side1 <= 0.0 || side2 <= 0.0 || side3 <= 0.0) {
+    throw std::invalid_argument("Triangle sides must all be positive.");
+  }
+  // Then, verify the triangle inequality holds.
+  if (side1 >= side2 + side3 || side2 >= side1 + side3 ||
+      side3 >= side1 + side2) {
+    throw std::invalid_argument("The triangle inequality must hold.");
+  }
+  // If the above hold, then just figure out what type of triangle it is.
   if (side1 == side2 && side2 == side3) {
     return TriangleType::EQUILATERAL;
-  } else if (side1 == side2 && side2 != side3) {
+  } else if (side1 != side2 && side2 != side3 && side3 != side1) {
+    return TriangleType::SCALENE;
+  } else if (side1 == side2 || side2 == side3 || side1 == side3) {
     return TriangleType::ISOSCELES;
   } else {
-    return TriangleType::SCALENE;
+    throw std::invalid_argument("Unable to determine the triangle type.");
   }
 }
 }  // namespace classify_triangle
