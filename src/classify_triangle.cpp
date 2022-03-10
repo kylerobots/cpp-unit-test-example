61ee9e13e615765540aa0977e774022f3e8f9f98
Kyle M. Hart <kylemhart@gmail.com>
2022-03-10 18:10:57 -0500

    Remove unreached case

--- src/classify_triangle.cpp
+++ src/classify_triangle.cpp
@@ -14,12 +14,10 @@ TriangleType classifyTriangle(double side1, double side2, double side3) {
   // If the above hold, then just figure out what type of triangle it is.
   if (side1 == side2 && side2 == side3) {
     return TriangleType::EQUILATERAL;
-  } else if (side1 != side2 && side2 != side3 && side3 != side1) {
-    return TriangleType::SCALENE;
   } else if (side1 == side2 || side2 == side3 || side1 == side3) {
     return TriangleType::ISOSCELES;
   } else {
-    throw std::invalid_argument("Unable to determine the triangle type.");
+    return TriangleType::SCALENE;
   }
 }
 }  // namespace classify_triangle
